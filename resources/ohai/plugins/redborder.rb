4799e92b0d8cff9e63ac09b747af63afba66d32c
flopez-redborder <flopez@redborder.com>
2026-09-08 13:40:08 +0100

    Release 2.8.0 (#45)
    * Add grr services (#43)
    
    Co-authored-by: Felix Lopez Da Silva <flopez@redborder.com>
    
    * Release 2.8.0
    
    ---------
    
    Co-authored-by: Vimesa <93142944+ViMesTor@users.noreply.github.com>

--- resources/ohai/plugins/redborder.rb
+++ resources/ohai/plugins/redborder.rb
@@ -99,7 +99,8 @@ Ohai.plugin(:Redborder) do
                   "airflow-dag-processor", "airflow-scheduler", "airflow-triggerer", "airflow-webserver",
                   "opscode-erchef", "postgresql", "nginx", "memcached", "n2klocd", "redborder-nmsp", "redis",
                   "opscode-bookshelf", "opscode-chef-mover", "opscode-rabbitmq", "http2k", "redborder-cep", "snmpd", "snmptrapd",
-                  "redborder-dswatcher", "redborder-events-counter", "sfacctd", "redborder-ale", "logstash", "mongod", "minio", "aerospike", "redborder-agents"]
+                  "redborder-dswatcher", "redborder-events-counter", "sfacctd", "redborder-ale", "logstash", "mongod", "minio", "aerospike", "redborder-agents",
+                  "grr-adminui", "grr-fleetspeak", "grr-frontend", "grr-worker"]
       services.each do |s|
         service_data = Mash.new
         service_data[:name] = s
